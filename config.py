1ad3a2089c991d179b8897cad7429ff5b5380b97
taey16 <taey1600@gmail.com>
2016-08-20 14:10:07 +0900

    mod babi10k config joint

--- config.py
+++ config.py
@@ -67,13 +67,13 @@ class BabiConfigJoint(object):
     self.dictionary = dictionary
     self.batch_size = 32
     self.nhops = 3
-    self.nepochs = 60 # 1K training samples XXX;
-    #self.nepochs = 20 # 10K training samples XXX;
+    self.nepochs = 60 # 1K training samples
+    #self.nepochs = 20 # 10K training samples
 
-    self.lrate_decay_step = 15   # 1k training sampels reduce learning rate by half every 15 epochs  # XXX:
-    #self.lrate_decay_step = 5   # 10k training sampels reduce learning rate by half every 5 epochs  # XXX:
+    self.lrate_decay_step = 15   # 1k training sampels reduce learning rate by half every 15 epochs 
+    #self.lrate_decay_step = 5   # 10k training sampels reduce learning rate by half every 5 epochs
 
-    # Use 10% of training data for validation  # XXX
+    # Use 10% of training data for validation
     nb_questions = train_questions.shape[1]
     nb_train_questions = int(nb_questions * 0.9)
 
@@ -97,17 +97,17 @@ class BabiConfigJoint(object):
     self.display_inteval = 10
 
     if self.linear_start:
-      self.ls_nepochs = 30  # XXX:
-      self.ls_lrate_decay_step = 31  # XXX:
-      self.ls_init_lrate = 0.01 / 2 # eta = 0.005 XXX
+      self.ls_nepochs = 30
+      self.ls_lrate_decay_step = 31
+      self.ls_init_lrate = 0.01 / 2 # eta = 0.005
 
     # Training configuration
     self.train_config = {
-      "init_lrate"   : 0.01,
-      "max_grad_norm": 40,
-      "in_dim"     : 50,  # XXX:
-      "out_dim"    : 50,  # XXX:
-      "sz"       : min(50, train_story.shape[1]),
+      "init_lrate"   : 0.01, # NOTE: it is ignored instead ls_init_lrate when using LS
+      "max_grad_norm": 40, # NOTE: clip gradient
+      "in_dim"     : 50,  # NOTE: soft-attention memory slot(sentence) dim
+      "out_dim"    : 50,  # NOTE: output memory slot(sentence) dim
+      "sz" : min(50, train_story.shape[1]), # The capacity of memory is restricted to the most recent 50 sentences.
       "voc_sz"     : len(self.dictionary),
       "bsz"      : self.batch_size,
       "max_words"  : len(train_story),
@@ -131,15 +131,14 @@ class Babi10kConfigJoint(object):
   def __init__(self, train_story, train_questions, dictionary):
     print('We will use Babi10kConfigJoint')
 
-    # TODO: Inherit from BabiConfig
     self.dictionary = dictionary
     self.batch_size = 32
     self.nhops = 3
-    self.nepochs = 30 # 1K training samples XXX;
+    self.nepochs = 100 # 1K training samples
 
-    self.lrate_decay_step = 6  # 1k training sampels reduce learning rate by half every 15 epochs  # XXX:
+    self.lrate_decay_step = 20  # 1k training sampels reduce learning rate by half every 15 epochs
 
-    # Use 10% of training data for validation  # XXX
+    # Use 10% of training data for validation
     nb_questions = train_questions.shape[1]
     nb_train_questions = int(nb_questions * 0.9)
 
@@ -156,28 +155,28 @@ class Babi10kConfigJoint(object):
     # the softmax layers were re-inserted and training recommenced.
     self.linear_start = True
     self.share_type = 1    # 1: adjacent, 2: layer-wise weight tying (RNN-style)
-    self.randomize_time = 0.0#0.1  # amount of noise injected into time index (Random Noise RN)
+    self.randomize_time = 0.1  # amount of noise injected into time index (Random Noise RN)
     self.add_proj = False  # add linear layer between internal states
     self.add_nonlin = False  # add non-linearity to internal states
 
     self.display_inteval = 10
 
     if self.linear_start:
-      self.ls_nepochs = 5  # XXX:
-      self.ls_lrate_decay_step = 6  # XXX:
-      self.ls_init_lrate = 0.01 # eta = 0.005 XXX
+      self.ls_nepochs = 5  # NOTE
+      self.ls_lrate_decay_step = 6 # NOTE
+      self.ls_init_lrate = 0.01 # NOTE: eta = 0.005
 
     # Training configuration
     self.train_config = {
-      "init_lrate" : 0.01,
-      "max_grad_norm" : 40,
-      "in_dim" : 50,  # XXX:
-      "out_dim" : 50,  # XXX:
+      "init_lrate"   : 0.01, # NOTE: it is ignored instead ls_init_lrate when using LS
+      "max_grad_norm": 40, # NOTE: clip gradient
+      "in_dim"     : 50,  # NOTE: soft-attention memory slot(sentence) dim
+      "out_dim"    : 50,  # NOTE: output memory slot(sentence) dim
       "sz" : min(50, train_story.shape[1]), # The capacity of memory is restricted to the most recent 50 sentences.
-      "voc_sz" : len(self.dictionary),
-      "bsz" : self.batch_size,
-      "max_words" : len(train_story),
-      "weight" : None
+      "voc_sz"     : len(self.dictionary),
+      "bsz"      : self.batch_size,
+      "max_words"  : len(train_story),
+      "weight"     : None
     }
 
     if self.linear_start:
@@ -187,4 +186,4 @@ class Babi10kConfigJoint(object):
       self.train_config.update({
         "voc_sz"   : self.train_config["voc_sz"] + self.train_config["sz"],
         "max_words": self.train_config["max_words"] + 1  # Add 1 for time words
-       })
+      })
