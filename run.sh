c0e8a0b2d470aad1886af310b9516450c51ceaef
taey16 <taey1600@gmail.com>
2016-10-10 17:08:52 +0900

    get rnd_seed 0 for best performance

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # for train
-python demo/qa.py -d /storage/babi/tasks_1-20_v1-2/en -l /storage/babi/trained_model/en_PE_LS_RN_joint_hop4/ -m memn2n.pkl -seed 2 --train
-python demo/qa.py -d /storage/babi/tasks_1-20_v1-2/en -l /storage/babi/trained_model/en_PE_LS_RN_joint_hop4/ -m memn2n.pkl -seed 2 --console
+python demo/qa.py -d /storage/babi/tasks_1-20_v1-2/en -l /storage/babi/trained_model/en_PE_LS_RN_joint_hop4/ -m memn2n.pkl -seed 0 --train
+python demo/qa.py -d /storage/babi/tasks_1-20_v1-2/en -l /storage/babi/trained_model/en_PE_LS_RN_joint_hop4/ -m memn2n.pkl -seed 0 --console
